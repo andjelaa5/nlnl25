b300952a9252d833208af54846d90a06bfef81a2
andjelaa5 <andelajankovic5@gmail.com>
2025-02-03 15:28:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,22 @@
-import csv
-from flask import Flask, render_template, request, jsonify, g
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
-import pandas as pd
-import json
-from bson import json_util 
+from bson import json_util
 import os
+import json
 
 app = Flask(__name__)
 CORS(app)  # Omogućava CORS
 
-# Funkcija za uspostavljanje konekcije sa bazom
+# Funkcija za konekciju sa bazom po potrebi
 def get_db():
-    if 'db' not in g:
-        client = MongoClient(
-            "mongodb+srv://user1:awd123faw13@cluster0.m9u9j.mongodb.net/test?retryWrites=true&w=majority",
-            tlsAllowInvalidCertificates=True,
-            socketKeepAlive=True  # Održava aktivnu konekciju
-        )
-        g.client = client
-        g.db = client['test']
-    return g.db
+    client = MongoClient(
+        "mongodb+srv://user1:awd123faw13@cluster0.m9u9j.mongodb.net/test?retryWrites=true&w=majority",
+        tlsAllowInvalidCertificates=True,
+        connect=False  # Onemogućava trajnu konekciju
+    )
+    return client['test']
 
-# Zatvaranje konekcije na kraju svakog zahteva
-@app.teardown_appcontext
-def close_db(error):
-    client = g.pop('client', None)
-    if client is not None:
-        client.close()
-
-# Ping pre svakog zahteva (provera da li je konekcija aktivna)
-@app.before_request
-def keep_connection_alive():
-    try:
-        db = get_db()
-        db.command('ping')
-    except:
-        g.pop('db', None)  # Ako je konekcija prekinuta, resetuj
-
-# Ruta za serviranje HTML forme
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -51,29 +29,24 @@ def form():
 def form2():
     return render_template('form2.html')
 
-# Ruta za primanje podataka sa forme i upisivanje u bazu
 @app.route('/submit', methods=['POST'])
 def submit_form():
     data = request.get_json()
-    
     if not data:
         return jsonify({"error": "No data received"}), 400
 
     db = get_db()
     collection = db['users']
-    
-    # Optimizovano brojanje dokumenata
     l = collection.estimated_document_count() + 1
 
     data["id"] = l
     collection.insert_one(data)
-    
+
     return jsonify({
         "message": "Podaci su uspešno sačuvani!",
         "broj": l
     })
 
-# Ruta za prikazivanje svih podataka
 @app.route('/form3')
 def form3():
     return render_template('form3.html')

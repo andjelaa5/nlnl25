19af9f1abfdfef62223feb64a016101900517889
andjelaa5 <andelajankovic5@gmail.com>
2025-02-03 15:22:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,42 @@
 import csv
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, g
 from flask_cors import CORS
 from pymongo import MongoClient
 import pandas as pd
 import json
 from bson import json_util 
 import os
-import threading
-import time
 
 app = Flask(__name__)
 CORS(app)  # Omogućava CORS
 
-client = MongoClient(
-    "mongodb+srv://user1:awd123faw13@cluster0.m9u9j.mongodb.net/test?retryWrites=true&w=majority",
-    tlsAllowInvalidCertificates=True  # Onemogućava proveru SSL sertifikata
-)
-db = client['test']
-collection = db['users']  # Ime kolekcije
-server_info = client.server_info()
+# Funkcija za uspostavljanje konekcije sa bazom
+def get_db():
+    if 'db' not in g:
+        client = MongoClient(
+            "mongodb+srv://user1:awd123faw13@cluster0.m9u9j.mongodb.net/test?retryWrites=true&w=majority",
+            tlsAllowInvalidCertificates=True,
+            socketKeepAlive=True  # Održava aktivnu konekciju
+        )
+        g.client = client
+        g.db = client['test']
+    return g.db
 
+# Zatvaranje konekcije na kraju svakog zahteva
+@app.teardown_appcontext
+def close_db(error):
+    client = g.pop('client', None)
+    if client is not None:
+        client.close()
 
-
-# Putanja do CSV fajla (proveri da li je fajl na pravoj lokaciji)
+# Ping pre svakog zahteva (provera da li je konekcija aktivna)
+@app.before_request
+def keep_connection_alive():
+    try:
+        db = get_db()
+        db.command('ping')
+    except:
+        g.pop('db', None)  # Ako je konekcija prekinuta, resetuj
 
 # Ruta za serviranje HTML forme
 @app.route('/')
@@ -33,50 +47,43 @@ def index():
 def form():
     return render_template('form.html')
 
-
 @app.route('/form2')
 def form2():
     return render_template('form2.html')
-# Ruta za primanje podataka sa forme i upisivanje u CSV
+
+# Ruta za primanje podataka sa forme i upisivanje u bazu
 @app.route('/submit', methods=['POST'])
 def submit_form():
-    # Dobij podatke sa forme
     data = request.get_json()
     
-    # Dobija podatke iz JS-a
     if not data:
         return jsonify({"error": "No data received"}), 400
-    l = collection.count_documents({}) + 1  # Brže brojanje
-    ime = data['ime']
-    prezime = data['prezime']
-    pol = data['pol']
-    zeljenipol = data['zeljenipol']
-    tiplicnosti = data['tiplicnosti']
-    roleModel = data['roleModel']
-    zivotnicilj = data['zivotnicilj']
-    zanr = data['zanr']
-    pice = data['pice']
-    hobi = data['hobi']
-    pesma = data['pesma']
-    zauzet = data['zauzet']
-    par = data['zauzet']
-    user_data = [l, ime, prezime, pol, zeljenipol, tiplicnosti, roleModel, zivotnicilj, zanr, pice, hobi, pesma, zauzet, par]
+
+    db = get_db()
+    collection = db['users']
+    
+    # Optimizovano brojanje dokumenata
+    l = collection.estimated_document_count() + 1
+
     data["id"] = l
     collection.insert_one(data)
+    
     return jsonify({
         "message": "Podaci su uspešno sačuvani!",
         "broj": l
     })
 
-# Ruta za prikazivanje svih podataka u CSV fajlu (JSON format)
+# Ruta za prikazivanje svih podataka
 @app.route('/form3')
 def form3():
     return render_template('form3.html')
 
 @app.route('/get_form3_data')
 def get_form3_data():
-    gas = list(collection.find())
-    return jsonify({"lista": json.loads(json_util.dumps(gas))})
-    
+    db = get_db()
+    collection = db['users']
+    data_list = list(collection.find())
+    return jsonify({"lista": json.loads(json_util.dumps(data_list))})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
